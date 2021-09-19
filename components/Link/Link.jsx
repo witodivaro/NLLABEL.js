d070e1d87429959bb2cdd45b37d69e3d595ef2b7
witodivaro <witodivaro@gmail.com>
2021-09-20 00:05:19 +0300

    fix: rename REACT_APP to NEXT_PUBLIC envs

--- components/Link/Link.jsx
+++ components/Link/Link.jsx
@@ -5,7 +5,7 @@ import styles from "./Link.module.scss";
 const Link = ({ className, children, color }) => {
   return (
     <a
-      href={process.env.REACT_APP_TELEGRAM_URL}
+      href={process.env.NEXT_PUBLIC_TELEGRAM_URL}
       target="_blank"
       rel="noreferrer"
       className={`${styles.link} ${styles[`link--${color}`]} ${className}`}
